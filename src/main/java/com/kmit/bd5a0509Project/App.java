abe77a17aa2b701e003b13a93202badd69b8f10b
sowmya17 <sowmyasree1702@gmail.com>
2022-11-22 12:41:12 +0530

    Demo for Test

---
+++ src/main/java/com/kmit/bd5a0509Project/App.java
@@ -0,0 +1,13 @@
+package com.kmit.bd5a0509Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
